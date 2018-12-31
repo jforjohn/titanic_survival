e14b6b171c656c104941f7306d16c88d0a5a6bdd
jforjohn <giannisotiro@gmail.com>
2018-12-31 12:13:01 +0200

    LAUNCHER: first structure

---
+++ MainLauncher.py
@@ -0,0 +1,79 @@
+##
+#from MyPreprocessing import MyPreprocessing
+import pandas as pd
+import numpy as np
+from config_loader import load, clf_names
+import argparse
+import sys
+import seaborn as sns
+from dataUnderstanding import featureAnalysis
+
+import numpy as np
+from time import time
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
+import matplotlib.pyplot as plt
+
+def getData(path, filenames_type):
+    '''
+    features_lst = [
+        "Pclass", "Survived", "Name", "Sex", "Age",
+        "Sibsp", "Parch", "Ticket", "Fare", "Cabin","Embarked",
+        "Boat","Body","Home.dest"]
+    '''
+    if filenames_type == 'train':
+        filename = 'train'
+    elif filenames_type == 'test':
+        filename = 'test'
+    else:
+        filename = 'titanicAll'
+    df_features = pd.read_csv(path + filename + '.csv',
+                           sep=',')
+
+    if filename not in ['train', 'test']:
+        # drop unnecessary columns that don't exist in the official data
+        df_features = df_features.drop(['Boat', 'Body', 'Home.dest'],
+                                       axis=1,
+                                       inplace=True)
+    #labels = df_features['Survived']
+    #df_features = df_features.drop(['Survived'], axis=1)
+    return df_features, labels
+##
+if __name__ == '__main__':
+    ##
+    # Loads config
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", default="titanic.cfg",
+        help="specify the location of the clustering config file"
+    )
+    args, _ = parser.parse_known_args()
+
+    config_file = args.config
+    config = load(config_file)
+
+    ##
+    verbose = config.get('titanic', 'verbose')
+    path = config.get('titanic', 'path') + '/'
+    file_type = config.get('titanic', 'file_type')
+
+    filename_type = 'train'
+    if file_type == 'all':
+        filename_type = 'other'
+
+    trainX, ytrain = getData(path, filename_type)
+
+    ## Data Understanding
+    featureAnalysis(trainX, verbose)
+    '''
+    ## Preprocessing
+    preprocess = MyPreprocessing()
+    preprocess.fit(trainX)
+    df = preprocess.new_df
+    labels = preprocess.labels_
+    '''
+
+    ##
+    start = time()
+
+
