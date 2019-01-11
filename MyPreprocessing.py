4d60151a2925513cb9a7f0daddedf8163d399bd2
jforjohn <giannisotiro@gmail.com>
2019-01-11 05:37:31 +0100

    PREPROCESSING: with full features

--- MyPreprocessing.py
+++ MyPreprocessing.py
@@ -13,12 +13,13 @@ class MyPreprocessing:
         pd.set_option('display.width', 1000)
         pd.options.mode.chained_assignment = None  # default='warn'
         self.filename_type = filename_type
+        self.new_df = pd.DataFrame()
 
 
 ##
     def handleMissingValues(self, data):
         # Fill the  NaN of Fare with the mean value
-        data.Fare.fillna(data.Fare.mean(), inplace=True)
+        data.Fare.fillna(data.Fare.median(), inplace=True)
 
         data.Embarked.fillna(data.Embarked.mode()[0], inplace=True)
 
@@ -31,7 +32,8 @@ class MyPreprocessing:
         # combination of the condition includes just NaN values
         data['Age'] = data.Age.fillna(
             data.groupby(['Title'])['Age'].transform('median'))
-
+        # for remaining NaNs
+        data['Age'] = data.Age.fillna(data.Age.mean())
         return data
 
     def replace_titles(self, x):
@@ -46,7 +48,7 @@ class MyPreprocessing:
             # The female doctor survives so goes with survival=1
             return 'Ms'
         # The male doctor survives so goes with survival=0.5
-        elif title in ['Dr', 'Master', 'Col', 'Major']:
+        elif title in ['Sir', 'Dr', 'Master', 'Col', 'Major']:
             return 'Mr50'
         # The male doctor survives so goes with survival=0
         elif title in ['Jonkheer', 'Don', 'Rev', 'Capt']:
@@ -55,61 +57,147 @@ class MyPreprocessing:
             # Sir always survives, Mr is 16% and Ms is 50% survival
             return title
 
-##
+    def get_ticket_id(row):
+        print(row)
+        ticket = row.Ticket
+        if ticket.isdigit():
+            return 'N'
+        else:
+            ticket_id = ticket.replace('.', '').replace('/', '').split()[0]
+            return ticket_id
+
+    def dropDependents(self):
+        self.new_df.drop(['Age', 'SibSp', 'Parch', 'Fare'], axis=1, inplace=True)
+
+    ##
     def fit(self, data):
         self.df_initial = self.handleMissingValues(data)
 
         # get label
-        if self.filename_type in ['train', 'other']:
+        if 'Survived' in data.columns:
             labels = self.df_initial.Survived
             self.labels_ = labels
             self.df_initial.drop(['Survived'], axis=1, inplace=True)
         else:
             self.labels_ = pd.Series()
 
-        df_reduced = self.df_initial.copy()
+        df = self.df_initial.copy()
 
+        # Title
+        df['Title'] = df.apply(self.replace_titles, axis=1)
+        df = pd.get_dummies(df, columns=['Title'], drop_first=True)
+
+        # Sex
+        # need to give males and females numeric values
+        df.loc[df["Sex"] == "male", "Sex"] = 0
+        df.loc[df["Sex"] == "female", "Sex"] = 1
+
+        # Age
+        # Various ages
+        df['Age_bin'] = pd.cut(df['Age'], bins=[-1, 2, 12, 17, 120],
+                               labels=['Infant', 'Kid', 'Teenager', 'Adult'])
+        df = pd.get_dummies(df, columns=['Age_bin'], drop_first=True)
+
+        # Family
+        df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+        # Family size is the sum of siblings, spouses, parents, and children.
+        df['Family_bin'] = pd.cut(df['FamilySize'], bins=[0, 1, 4, 7, 12],
+                                  labels=['Single', 'SmallFamily', 'BigFamily', 'Team'])
+        df = pd.get_dummies(df, columns=['Family_bin'], drop_first=True)
+
+        # Fare
+        df['Fare_bin'] = pd.cut(df['Fare'],
+                                bins=[0, 7.91, 14.45, 31, 120],
+                                labels=['Low', 'Median', 'Average', 'High'])
+        df = pd.get_dummies(df, columns=['Fare_bin'], drop_first=True)
+
+        # Embarked
+        df = pd.get_dummies(df, columns=["Embarked"], prefix="Em")
+
+        # Cabin
+        # Replace the Cabin number by the type of cabin 'X' if not
+        df['Cabin'] = df['Cabin'].str.extract("([a-zA-Z]+)", expand=True)
+        df['Cabin'] = df['Cabin'].fillna("X")
+        df = pd.get_dummies(df, columns=["Cabin"], prefix="Cabin")
+
+        # Ticket
+        # Find the length of the ticket, perhaps longer tickets are for include more amenities,
+        # therefore associated with wealth.
+        # df['TicketLength'] = df['Ticket'].map(lambda x: len(x))
+        # df['TicketL_bin'] = pd.cut(df['TicketLength'], bins=[0,5,8,10,20], labels=['Small','Average',
+        #                                                                      'Big','Huge'])
+        # df = pd.get_dummies(df, columns=["TicketL_bin"])
+        # df['TicketID'] = df.apply(get_ticket_id, axis=1)
+        # df['TicketID'] = df.apply(get_ticket_id, axis=1)
+
+        # Extras
+
+        # To be a parent, you need a spouse, have at least 1 child, and be older than 18.
+        # SibSp is >= 0 because you could be an adult with a sibling on board.
+        parent = (df['Parch'] > 0) & (df['Age'] >= 18)
+
+        # To be a mother, you need to be a parent and female. Fathers = Parent & Male
+        df['Mother'] = ((parent == 1) & (df['Sex'] == 1)).map(lambda s: 1 if s else 0)
+        df['Father'] = ((parent == 1) & (df['Sex'] == 0)).map(lambda s: 1 if s else 0)
+
+        # Child has at least 1 parent and is 17 or younger
+        child = (df['Parch'] >= 1) & (df['Age'] <= 17)
+
+        # To be a daughter, you need to be a girl, and a child.
+        # To be a son, likewise, but a boy.
+        df['Daughter'] = ((df['Sex'] == 1) & (child == 1)).map(lambda s: 1 if s else 0)
+        df['Son'] = ((df['Sex'] == 0) & (child == 1)).map(lambda s: 1 if s else 0)
+
+        # Orphan if you have no parents and are 17 or younger
+        df['Orphan'] = ((df['Age'] <= 17) & (df['Parch'] == 0)).map(lambda s: 1 if s else 0)
+
+        # Combined class and gender to better organize people.
+        df['RichWoman'] = ((df['Pclass'] == 1) & (df['Sex'] == 1) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+        df['MiddleClassWoman'] = ((df['Pclass'] == 2) & (df['Sex'] == 1) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+        df['PoorWoman'] = ((df['Pclass'] == 3) & (df['Sex'] == 1) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+        df['RichMan'] = ((df['Pclass'] == 1) & (df['Sex'] == 0) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+        df['MiddleClassMan'] = ((df['Pclass'] == 2) & (df['Sex'] == 0) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+        df['PoorMan'] = ((df['Pclass'] == 3) & (df['Sex'] == 0) & (df['Age'] >= 18)).map(
+            lambda s: 1 if s else 0)
+
+        df['RichGirl'] = ((df['Pclass'] == 1) & (df['Age'] <= 17) & (df['Sex'] == 1)).map(
+            lambda s: 1 if s else 0)
+        df['MiddleClassGirl'] = ((df['Pclass'] == 2) & (df['Age'] <= 17) & (df['Sex'] == 1)).map(
+            lambda s: 1 if s else 0)
+        df['PoorGirl'] = ((df['Pclass'] == 3) & (df['Age'] <= 17) & (df['Sex'] == 1)).map(
+            lambda s: 1 if s else 0)
+        df['RichBoy'] = ((df['Pclass'] == 1) & (df['Age'] <= 17) & (df['Sex'] == 0)).map(
+            lambda s: 1 if s else 0)
+        df['MiddleClassBoy'] = ((df['Pclass'] == 2) & (df['Age'] <= 17) & (df['Sex'] == 0)).map(
+            lambda s: 1 if s else 0)
+        df['PoorBoy'] = ((df['Pclass'] == 3) & (df['Age'] <= 17) & (df['Sex'] == 0)).map(
+            lambda s: 1 if s else 0)
+
+        # Pclass
+        df["Pclass"] = df["Pclass"].astype("object")
+        df = pd.get_dummies(df, columns=['Pclass'], drop_first=True)
+
+        # Drops
         # Remove columns
         # titanicAll dataset doesn't have PassengerId column
         if 'PassengerId' in data.columns:
-            df_reduced.drop(['PassengerId'], axis=1, inplace=True)
-        df_reduced.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+            df.drop(['PassengerId'], axis=1, inplace=True)
+        df.drop(['Name', 'Ticket', 'Fare'], axis=1, inplace=True)
 
-        # Pclass is ordinal so we convert it in object
-        df_reduced['Pclass'] = df_reduced['Pclass'].astype('object')
-        df_num = df_reduced.select_dtypes(exclude='object')
-        df_obj = df_reduced.select_dtypes(include='object')
+        df_num = df.select_dtypes(exclude='object')
+        df_obj = df.select_dtypes(include='object')
 
-        df_normalized = pd.DataFrame()
-        df_encoded = pd.DataFrame()
-        # normalize numerical data
         if df_num.size > 0:
             min_max_scaler = preprocessing.MinMaxScaler()
-            scaled = min_max_scaler.fit_transform(df_num.values.astype(float))
-            df_normalized = pd.DataFrame(scaled, columns=df_num.columns)
+            scaled = min_max_scaler.fit_transform(df.values.astype(float))
+            df_num = pd.DataFrame(scaled, columns=df.columns)
 
-        if df_obj.size > 0:
-            '''
-            # Sex is the only column that has 2 values so we can factorize it
-            df_obj['Sex'] = pd.factorize(df_obj['Sex'])[0]
-            sex = pd.DataFrame(df_obj.Sex, columns=['Sex'])
-            df_obj.drop(['Sex'], axis=1, inplace=True)
-            '''
-            # replace titles
-            df_obj['Title'] = df_obj.apply(self.replace_titles, axis=1)
+        self.new_df = pd.concat([df_obj, df_num], axis=1, sort=False)
 
-            # we use One-Hot encoding with all the categorical values
-            # Sex won't have 2 columns (1 for male and one for female)
-            # because we use the drop_first option
-            df_encoded = pd.get_dummies(df_obj,
-                                        drop_first=True)
-            # we convert them in float to avoid a warning
-            df_encoded = df_encoded.astype('float')
-            min_max_scaler = preprocessing.MinMaxScaler()
-            scaled = min_max_scaler.fit_transform(df_encoded)
-            df_encoded = pd.DataFrame(scaled, columns=df_encoded.columns)
-
-        self.new_df = pd.concat([df_normalized, df_encoded], axis=1, sort=False)
-#
 #plt.interactive(False)
 #plt.show(block=True)
